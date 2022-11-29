59fc11ab1d894439e4f6ce20c3ced89d2daed9f6
Michael Buchar <michaelbuchar0@gmail.com>
2022-11-28 21:27:17 -0500

    Update get_all_articles

--- src/controller.py
+++ src/controller.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 def get_user_input_topics():
     # List topics enumeration to user and ask to add to favorites
     return []
@@ -6,6 +9,19 @@ def get_user_input_topics():
 def get_all_articles(topic):
     topic.use()
     # Fetch all articles for topic (web scrape), returns a set
+
+    response = requests.get(
+        url="https://www.britannica.com/browse/Entertainment-Pop-Culture", timeout='10s'
+    )
+    soup = BeautifulSoup(response.content, 'html.parser')
+    for a in soup.find_all('a', href=True):
+        isStory = a['href'].startswith('/story/') or a['href'].startswith('/list/')
+        if isStory:
+            title = a.text
+            if title is not None:
+                # if title.contains(' '):
+                #     print ('nothing')
+                print (title.strip())
     return None
 
 
