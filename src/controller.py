99556aeefb384c7b743d6f0d0e9051b9c5e62d00
rodolphebaladi <rodolphebaladi@gmail.com>
2022-11-28 23:55:23 -0500

    fixed make

--- src/controller.py
+++ src/controller.py
@@ -1,37 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
 from topics import Topics
+from article import Article
 
 # List topics enumeration to user and ask to add to favorites
 def get_user_input_topics():
     # TODO: make sure two topics are not repeated
     exists_input = True
     list_of_topics = []
-    prompt_topic = str("Please enter a topic (number) you are interested in from the selection below:\n" + 
-                    "1. ENTERTAINMENT AND POP CULTURE\n" +
-                    "2. GEOGRAPHY AND TRAVEL\n" +
-                    "3. HEALTH AND MEDICINE\n" +
-                    "4. LIFESTYLES AND SOCIAL ISSUES\n" +
-                    "5. LITERATURE\n" +
-                    "6. PHILOSOPHY AND RELIGION\n" +
-                    "7. POLITICS, LAW AND GOVERNMENT\n" +
-                    "8. SCIENCE\n" +
-                    "9. SPORTS AND RECREATION\n" +
-                    "10. TECHNOLOGY\n" +
-                    "11. VISUAL ARTS\n" +
-                    "12. WORLD HISTORY\n" +
-                    "Enter a number: ")
+    prompt_topic1 = (
+        "Please enter a topic (number) you are interested in from the selection below:\n" + 
+        "1. ENTERTAINMENT AND POP CULTURE\n" +
+        "2. GEOGRAPHY AND TRAVEL\n" +
+        "3. HEALTH AND MEDICINE\n" +
+        "4. LIFESTYLES AND SOCIAL ISSUES\n" +
+        "5. LITERATURE\n" +
+        "6. PHILOSOPHY AND RELIGION\n" +
+        "7. POLITICS, LAW AND GOVERNMENT\n" +
+        "8. SCIENCE\n" +
+        "9. SPORTS AND RECREATION\n" +
+        "10. TECHNOLOGY\n" +
+        "11. VISUAL ARTS\n" +
+        "12. WORLD HISTORY\n"
+    )
+    prompt_topic2 = "Enter a number: "
+
     while (exists_input):
-        topic = input(prompt_topic)
+        # If first time input list all options, otherwise only take input
+        if len(list_of_topics) == 0:
+            topic = input(prompt_topic1 + prompt_topic2)
+        else:
+            topic = input(prompt_topic2)
+
+        # Validate input
         if (not topic.isnumeric()):
             print("\nPlease provide a number!\n") 
         elif (int(topic) < 1 or int(topic) > 13):
             print("\nInvalid number, please provide a number between 1 and 13!\n")
         else:
             list_of_topics.append(int(topic))
-            more_input = input("Would you like to provide another topic? (yes/no) ")
+            more_input = input("Would you like to provide another topic? [yes/other is no] ")
+        
             if (more_input != 'yes'):
                 exists_input = False
+        
     return list_of_topics
 
 
@@ -61,13 +73,13 @@ def get_all_articles(topic):
             if title is not None:
                 title = title.strip()
                 if (title != "" and title != "Lists"):
-                    articles.add(title)
+                    url = None # TODO: scrape the article url
+                    article = Article(title, topic, url)
+                    articles.add(article)
     
-    print(articles)
-    return None
+    return articles
 
 
 def get_random(articles):
-    articles.use()
     # Select a random article from set and remove it from the set
-    return 1
+    return articles.pop()
